506274cd77c2b21d719cd96930275e44e66d86f2
SuriThipu <75787680+SuriThipu@users.noreply.github.com>
2020-12-28 23:40:14 +0530

    Update Drivewheel.py
    updating header changes

--- Drivewheel.py
+++ Drivewheel.py
@@ -1,15 +1,8 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import Header as m
 
-from matplotlib.pyplot import xticks
-
-df = pd.DataFrame(pd.read_csv("CarPrice_Assignment.csv"))
-
-df_drivewheel_avg_price = df[['drivewheel','price']].groupby("drivewheel", as_index = False).mean().rename(columns={'price':'drivewheel_avg_price'})
-plt1 = df_drivewheel_avg_price.plot(x = 'drivewheel', kind='bar', sort_columns = True,legend = False,)
-plt1.set_xlabel("Drive Wheel Type")
-plt1.set_ylabel("Avg Price (Dollars)")
-xticks(rotation = 0)
-plt.show()
+m.df_drivewheel_avg_price = m.df[['drivewheel','price']].groupby("drivewheel", as_index = False).mean().rename(columns={'price':'drivewheel_avg_price'})
+m.plt1 = m.df_drivewheel_avg_price.plot(x = 'drivewheel', kind='bar', sort_columns = True,legend = False,)
+m.plt1.set_xlabel("Drive Wheel Type")
+m.plt1.set_ylabel("Avg Price (Dollars)")
+m.xticks(rotation = 0)
+m.plt.show()
