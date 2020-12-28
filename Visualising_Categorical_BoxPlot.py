bac0cc13af5a8366102e9f5aa7fbee7e592c315a
SuriThipu <75787680+SuriThipu@users.noreply.github.com>
2020-12-28 23:45:28 +0530

    Update Visualising_Categorical_BoxPlot.py
    updating header

--- Visualising_Categorical_BoxPlot.py
+++ Visualising_Categorical_BoxPlot.py
@@ -1,46 +1,40 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from matplotlib.pyplot import xticks
-
-df = pd.DataFrame(pd.read_csv("CarPrice_Assignment.csv"))
+import Header as m
 
 
-df['brand'] = df.CarName.str.split(' ').str.get(0).str.upper()
 
-df_comp_avg_price = df[['brand','price']].groupby("brand", as_index = False).mean().rename(columns={'price':'brand_avg_price'})
+m.df['brand'] = m.df.CarName.str.split(' ').str.get(0).str.upper()
 
-df = df.merge(df_comp_avg_price, on = 'brand')
+m.df_comp_avg_price = m.df[['brand','price']].groupby("brand", as_index = False).mean().rename(columns={'price':'brand_avg_price'})
 
-df['brand_category'] = df['brand_avg_price'].apply(lambda x : "Budget" if x < 10000 
+m.df = m.df.merge(m.df_comp_avg_price, on = 'brand')
+
+m.df['brand_category'] = m.df['brand_avg_price'].apply(lambda x : "Budget" if x < 10000 
                                                      else ("Mid_Range" if 10000 <= x < 20000
                                                            else "Luxury"))
 
 
 
-df['mileage'] = df['citympg']*0.55 + df['highwaympg']*0.45
+m.df['mileage'] = m.df['citympg']*0.55 + m.df['highwaympg']*0.45
 
 
-auto = df[['fueltype', 'aspiration', 'carbody', 'drivewheel', 'wheelbase', 'carlength', 'carwidth', 'curbweight', 'enginetype',
+auto = m.df[['fueltype', 'aspiration', 'carbody', 'drivewheel', 'wheelbase', 'carlength', 'carwidth', 'curbweight', 'enginetype',
        'cylindernumber', 'enginesize',  'boreratio', 'horsepower', 'price', 'brand_category', 'mileage']]
 
 
-plt.figure(figsize=(10, 20))
-plt.subplot(4,2,1)
-sns.boxplot(x = 'fueltype', y = 'price', data = auto)
-plt.subplot(4,2,2)
-sns.boxplot(x = 'aspiration', y = 'price', data = auto)
-plt.subplot(4,2,3)
-sns.boxplot(x = 'carbody', y = 'price', data = auto)
-plt.subplot(4,2,4)
-sns.boxplot(x = 'drivewheel', y = 'price', data = auto)
-plt.subplot(4,2,5)
-sns.boxplot(x = 'enginetype', y = 'price', data = auto)
-plt.subplot(4,2,6)
-sns.boxplot(x = 'brand_category', y = 'price', data = auto)
-plt.subplot(4,2,7)
-sns.boxplot(x = 'cylindernumber', y = 'price', data = auto)
-plt.tight_layout()
-plt.show()
+m.plt.figure(figsize=(10, 20))
+m.plt.subplot(4,2,1)
+m.sns.boxplot(x = 'fueltype', y = 'price', data = auto)
+m.plt.subplot(4,2,2)
+m.sns.boxplot(x = 'aspiration', y = 'price', data = auto)
+m.plt.subplot(4,2,3)
+m.sns.boxplot(x = 'carbody', y = 'price', data = auto)
+m.plt.subplot(4,2,4)
+m.sns.boxplot(x = 'drivewheel', y = 'price', data = auto)
+m.plt.subplot(4,2,5)
+m.sns.boxplot(x = 'enginetype', y = 'price', data = auto)
+m.plt.subplot(4,2,6)
+m.sns.boxplot(x = 'brand_category', y = 'price', data = auto)
+m.plt.subplot(4,2,7)
+m.sns.boxplot(x = 'cylindernumber', y = 'price', data = auto)
+m.plt.tight_layout()
+m.plt.show()
