963656bab752ca75d22fd8568a146d6338ffd1c8
SuriThipu <75787680+SuriThipu@users.noreply.github.com>
2020-12-28 23:44:13 +0530

    Update LinearRegressionModel.py
    Updating header

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -1,29 +1,23 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import Header as m
 
-from matplotlib.pyplot import xticks
 
-df = pd.DataFrame(pd.read_csv("CarPrice_Assignment.csv"))
+m.df['brand'] = m.df.CarName.str.split(' ').str.get(0).str.upper()
 
-df['brand'] = df.CarName.str.split(' ').str.get(0).str.upper()
+m.df_comp_avg_price = m.df[['brand','price']].groupby("brand", as_index = False).mean().rename(columns={'price':'brand_avg_price'})
 
-df_comp_avg_price = df[['brand','price']].groupby("brand", as_index = False).mean().rename(columns={'price':'brand_avg_price'})
+m.df = m.df.merge(m.df_comp_avg_price, on = 'brand')
 
-df = df.merge(df_comp_avg_price, on = 'brand')
-
-df['brand_category'] = df['brand_avg_price'].apply(lambda x : "Budget" if x < 10000 
+m.df['brand_category'] = m.df['brand_avg_price'].apply(lambda x : "Budget" if x < 10000 
                                                      else ("Mid_Range" if 10000 <= x < 20000
                                                            else "Luxury"))
 
 
 
-df['mileage'] = df['citympg']*0.55 + df['highwaympg']*0.45
+m.df['mileage'] = m.df['citympg']*0.55 + m.df['highwaympg']*0.45
 
-auto = df[['fueltype', 'aspiration', 'carbody', 'drivewheel', 'wheelbase', 'carlength', 'carwidth', 'curbweight', 'enginetype',
+auto = m.df[['fueltype', 'aspiration', 'carbody', 'drivewheel', 'wheelbase', 'carlength', 'carwidth', 'curbweight', 'enginetype',
        'cylindernumber', 'enginesize',  'boreratio', 'horsepower', 'price', 'brand_category', 'mileage']]
 
-plt.figure(figsize=(15, 15))
-sns.pairplot(auto)
-plt.show()
+m.plt.figure(figsize=(15, 15))
+m.sns.pairplot(auto)
+m.plt.show()
