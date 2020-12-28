39479bb2873cff796e754488499542bcab6b3323
SuriThipu <75787680+SuriThipu@users.noreply.github.com>
2020-12-28 18:45:01 +0530

    Update Aspiration.py
    Changing the header part

--- Aspiration.py
+++ Aspiration.py
@@ -1,18 +1,8 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import Header as m
 
-from matplotlib.pyplot import xticks
-
-df = pd.DataFrame(pd.read_csv("CarPrice_Assignment.csv"))
-
-
-
-
-df_aspir_avg_price = df[['aspiration','price']].groupby("aspiration", as_index = False).mean().rename(columns={'price':'aspir_avg_price'})
-plt1 = df_aspir_avg_price.plot(x = 'aspiration', kind='bar',legend = False, sort_columns = True)
-plt1.set_xlabel("Aspiration")
-plt1.set_ylabel("Avg Price (Dollars)")
-xticks(rotation = 0)
-plt.show()
+m.df_aspir_avg_price = m.df[['aspiration','price']].groupby("aspiration", as_index = False).mean().rename(columns={'price':'aspir_avg_price'})
+m.plt1 = m.df_aspir_avg_price.plot(x = 'aspiration', kind='bar',legend = False, sort_columns = True)
+m.plt1.set_xlabel("Aspiration")
+m.plt1.set_ylabel("Avg Price (Dollars)")
+m.xticks(rotation = 0)
+m.plt.show()
